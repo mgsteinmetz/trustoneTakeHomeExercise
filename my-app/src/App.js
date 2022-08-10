26509235b0c984525b6ea9e515f6e03065d0eb90
Mike Steinmetz <mikesteinmetz00@gmail.com>
2022-08-10 16:19:27 -0500

    styling form and starting on api fetching

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,53 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, useState } from 'react';
+import ReactDOM, { render } from 'react-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Container, Row, Col, Button, Card, FormGroup } from 'react-bootstrap';
 
+class County extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+    };
+  };
+
+  componentDidMount() {
+    fetch('https://arcgis.metc.state.mn.us/server/rest/services/GISLibrary/VWParcelsPoints/FeatureServer/0')
+    .then((res) => res.json())
+    .then((json) => {
+      this.setState({
+        items: json,
+        DataisLoaded: true,
+      });
+    });
+  };
+
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded) return 
+    <div>
+        <h1> Please wait, for content is loading...</h1>
+    </div>;
+    
+    return (
+      <div className='App'>
+        <h1> Fetching data...??</h1> {
+          items.map((item) => (
+            <ol key = {item.id }>
+              Owner: { item.owner },
+              Address: { item.address },
+              County: { item.county },
+              Market_Value: { item.value },
+            </ol>
+          ))
+        }
+      </div>
+    )
+  };
+};
 
 const CountyForm = (props) => {
   // const [formState, setFormState] = useState({ countyPin: ''});
@@ -44,28 +88,24 @@ const CountyForm = (props) => {
     // });
 
     return (
-      <main className="flex-row justify-center mb-4">
+      <main className="flex-row justify-center mb-4 p-5 text-center">
         <Container>
           <Row>
         <Col className="d-flex justify-content-center">
           <Card className="card col-md-6">
-            <h4 className="card-header bg-dark text-light p-2">Search County</h4>
+            <h4 className="card-header bg-dark text-light p-2 text-center">Search For Your County</h4>
             <Card.Body className="card-body">
                 <Form>
                   <Form.Group>
-                    <Form.Label>County Pin</Form.Label>
+                    <Form.Label>Please Enter a County PIN</Form.Label>
                     <Form.Control
                       className="form-input"
-                      placeholder="Enter County pin #"
+                      placeholder="enter county PIN#"
                       name="pin"
                       type="pin"
                     />
                   </Form.Group>
-                  <Button
-                    className="btn btn-block btn-primary"
-                    style={{ cursor: 'pointer' }}
-                    type="submit"
-                  >
+                  <Button className="btn btn-block btn-primary m-3 text-center" style={{ cursor: 'pointer' }} type="submit">
                     Search
                   </Button>
                 </Form>
